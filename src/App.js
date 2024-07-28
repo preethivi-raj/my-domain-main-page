1d83a1110fec75728dda8b1e5ab8e504b6c5ed66
PREETHIVIRAJ <preethiviraj9943@gmail.com>
2024-07-28 11:17:22 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='text-8xl font-serif p-4'>Preethiviraj M</h1>
+        <email>preethiviraj9943@gmail.com</email>
+      </header>
+    </div>
+  );
+}
+
+export default App;
